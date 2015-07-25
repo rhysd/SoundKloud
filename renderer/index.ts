d1e5e8841635b0e061c20d37061610d5d7446a9e
rhysd <lin90162@yahoo.co.jp>
2015-07-25 18:40:04 +0900

    do not popup login window

--- renderer/index.ts
+++ renderer/index.ts
@@ -2,9 +2,19 @@
 
 require('electron-cookies');
 
-var webview = document.getElementById('main-view');
+onload = function(){
+    let webview = document.getElementById('main-view');
 
-// Debug
-// webview.addEventListener('dom-ready', function(){
-//     webview.openDevTools();
-// });
+    // Debug
+    // webview.addEventListener('dom-ready', function(){
+    //     webview.openDevTools();
+    // });
+
+    webview.addEventListener('new-window', function(e: any){
+        console.log('guest window tries to open new window: ' + e.url);
+        if (e.url.startsWith('https://soundcloud.com/connect')) {
+            webview.src = 'https://soundcloud.com/connect?client_id=c259bb0ada8692d041954ac79e5eb394&response_type=token&redirect_uri=https%3A//soundcloud.com';
+        }
+    });
+}
+
--- renderer/lib.d.ts
+++ renderer/lib.d.ts
@@ -2,4 +2,5 @@ declare function require(mod: string): any; // tsurai
 
 interface HTMLElement {
     openDevTools: () => void;
+    src: string;
 }
